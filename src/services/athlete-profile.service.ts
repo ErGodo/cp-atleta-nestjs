d0b738e718055dc45345851289ffbb9e82c5e5c4
ErGodo <wadhgodo@gmail.com>
2026-01-15 10:52:45 -0300

    fix de error handling

--- src/services/athlete-profile.service.ts
+++ src/services/athlete-profile.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AthleteProfile } from '../athlete-profile/athlete-profile.entity';
@@ -8,11 +8,21 @@ export class AthleteProfileService {
   constructor(
     @InjectRepository(AthleteProfile)
     private athleteProfileRepository: Repository<AthleteProfile>,
-  ) {}
+  ) { }
 
   async create(athleteProfileData: Partial<AthleteProfile>): Promise<AthleteProfile> {
-    const athleteProfile = this.athleteProfileRepository.create(athleteProfileData);
-    return this.athleteProfileRepository.save(athleteProfile);
+    try {
+      const athleteProfile = this.athleteProfileRepository.create(athleteProfileData);
+      return await this.athleteProfileRepository.save(athleteProfile);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Athlete profile already exists for this athlete');
+      }
+      if (error.code === '23503') {
+        throw new BadRequestException('Athlete ID not found');
+      }
+      throw new InternalServerErrorException('Failed to create athlete profile: ' + error.message);
+    }
   }
 
   async findAll(): Promise<AthleteProfile[]> {
